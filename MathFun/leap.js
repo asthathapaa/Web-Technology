1221b818243a808e54a2a7fdd98ec3d323ca34e2
Astha Thapa <119739490+asthathapaa@users.noreply.github.com>
2024-05-19 20:40:17 +0545

    Add files via upload

---
+++ MathFun/leap.js
@@ -0,0 +1,20 @@
+
+function isLeapYear(year) {
+    if (year % 4 !== 0) {
+        return false;
+    } else if (year % 100 !== 0) {
+        return true;
+    } else if (year % 400 !== 0) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+// Example usage:
+const year = 2024;
+if (isLeapYear(year)) {
+    console.log(year + " is a leap year.");
+} else {
+    console.log(year + " is not a leap year.");
+}
